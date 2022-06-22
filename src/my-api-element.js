c3a9a82cdcd3282354a9faf64fb796dea9b082ce
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-22 14:51:37 -0500

    add my-elemento into my-api-element

--- src/my-api-element.js
+++ src/my-api-element.js
@@ -1,13 +1,16 @@
 import { LitElement, html } from "lit-element";
+//import "./my-element"
 
 class MyApiElement extends LitElement{
     static get properties(){
         return {
             data: {type: Array},
+            loading: {type: Boolean},
         }
     }
     constructor(){
         super();
+        this.loading = true;
     }
     connectedCallback() {
         super.connectedCallback();
@@ -17,8 +20,12 @@ class MyApiElement extends LitElement{
         }
     }
     render(){
+        if(this.loading) {
+            return html`<p>loading...</p>`
+        }
         return html`
         <p>Vamos a llamar una API</p>
+        <my-element></my-element>
         <ul>
             ${this.data.map(item => html`<li>${item.name}</li>`)}
         </ul>
@@ -29,10 +36,11 @@ class MyApiElement extends LitElement{
 
 
     async fetchBreweries() {
+        this.loading = true;
         const response = await fetch('https://api.openbrewerydb.org/breweries');
         const jsonResponse = await response.json();
         this.data = jsonResponse;
-        console.log(this.data);
+        this.loading = false;
     }
 }
 
